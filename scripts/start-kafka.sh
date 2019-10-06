b450d1c676924a91e2318e29e4640ea10f8835ff
Serdar Kuzucu <serdarkuzucu13@gmail.com>
2019-10-07 00:30:28 +0300

    Append new line to server.properties before appending new options

--- scripts/start-kafka.sh
+++ scripts/start-kafka.sh
@@ -8,6 +8,8 @@
 # * LOG_RETENTION_BYTES: configure the size at which segments are pruned from the log, (default is 1073741824, for 1GB)
 # * NUM_PARTITIONS: configure the default number of log partitions per topic
 
+echo "" >> $KAFKA_HOME/config/server.properties
+
 # Configure advertised host/port if we run in helios
 if [ ! -z "$HELIOS_PORT_kafka" ]; then
     ADVERTISED_HOST=`echo $HELIOS_PORT_kafka | cut -d':' -f 1 | xargs -n 1 dig +short | tail -n 1`
